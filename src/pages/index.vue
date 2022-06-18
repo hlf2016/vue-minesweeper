3b9fdc6c1423c744d72e1405b3715297a2bd72e2
Pannio <1821299140@qq.com>
2022-06-18 23:14:49 +0800

    添加 readme 分离 BlockState 为单独的ts文件

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,24 +1,13 @@
 <script setup lang="ts">
 
+// 引入扫雷块 接口
+import { BlockState } from '~/types'
+
 // 标记是否当前开发模式
 let dev = false;
 
 // 标识是否已经生成炸弹分布
 let mineGenerated = false;
-
-// 一个 块 的状态 接口
-interface BlockState {
-  x: number;
-  y: number;
-  // 是否被翻过
-  revealed?: boolean;
-  // 是否是炸弹
-  mine?: boolean;
-  // 是否被标记为小旗帜
-  flagged?: boolean;
-  // 相邻的炸弹数目
-  adjacentMines: number;
-}
 
 const WIDTH = 10;
 const HEIGHT = 10;
